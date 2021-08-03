a6dcabcce92b6a1bb59d97ecc3ad5880b8ed373c
Abril Palencia <pal18198@uvg.edu.gt>
2021-08-03 17:22:20 -0600

    bitarray

--- Cliente/Cliente.py
+++ Cliente/Cliente.py
@@ -6,12 +6,15 @@
 
 # Libreria socket
 from socket import *
+from encode import Encode_bit 
+
+encode_bit = Encode_bit()
 
 serverIP = "192.168.1.54"
 serverPort =  12000
 clienteSocket = socket(AF_INET, SOCK_DGRAM)
-mensaje = input("escriba el mensaje: ")
-clienteSocket.sendto(mensaje.encode(), (serverIP, serverPort))
+mensaje = input()
+clienteSocket.sendto(encode_bit.encode_cadena(mensaje), (serverIP, serverPort))
 mensajeCodificado, serverAddress = clienteSocket.recvfrom(2048)
 print(mensajeCodificado.decode())
 clienteSocket.close()
---
+++ Cliente/encode.py
@@ -0,0 +1,18 @@
+# Laboratorio No.2, encode.py
+# 29/07/2021
+# Integrantes:
+#   - Abril Palencia, 18198
+#   - Cristina Bautista, 161260
+
+#librerias
+from bitarray import bitarray
+
+class Encode_bit():
+    def __init__(self):
+        self.mensaje_codificado = ''
+
+    def encode_cadena(self, mensaje):
+        bit = bitarray()
+        bit.frombytes(mensaje.encode())
+        self.mensaje_codificado = str(bit.to01())
+        print(self.mensaje_codificado)
